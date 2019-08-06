a2547934646e721c8e0a3af06ad38ccc27c2e0ef
Max-M <maxnmagura@gmail.com>
2019-08-05 21:26:37 -0500

    added defer to script tag in html to have js run properly

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+
+var winsNumber = document.getElementById("wins-number");
+var lossesNumber = document.getElementById("losses-number");
+var guessesNumber = document.getElementById("guesses-number");
+var guessesText = document.getElementById("guesses-text");
+
+document.onkeyup = function(event) {
+
+  var userGuess = event.key;
+
+  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+  if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
+
+      if (userGuess === computerGuess) {
+        wins++;
+      } 
+
+      else {
+        guesses--
+      }
+    }
+
+  if (guesses === 0) {
+    losses++
+  }
+
+  winsNumber.textContent = wins;
+  lossesNumber.textContent = losses;
+  guessesNumber.textContent = guesses;
+  
+};
+
